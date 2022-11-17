54574775c98eb1fae09f757073231a30efe7cedc
johnwairia <wairia21@gmail.com>
2022-11-17 13:17:52 +0300

    update

--- app/src/main/java/com/jkimtech/bursaryadmin/AdminAdapterDialog.java
+++ app/src/main/java/com/jkimtech/bursaryadmin/AdminAdapterDialog.java
@@ -1,4 +1,188 @@
 package com.jkimtech.bursaryadmin;
 
-public class AdminAdapterDialog {
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminAdapterDialog extends DialogFragment {
+    private RecyclerView recyclerView;
+    private AdminAdapter adapter;
+    private static List<Upload> uploadList = new ArrayList<>();
+    private ValueEventListener rejectListener, acceptListener;
+    private MaterialToolbar toolbar;
+
+    public static void showDialog(FragmentManager manager, List<Upload> uploads) {
+        AdminAdapterDialog dialog = new AdminAdapterDialog();
+        uploadList.clear();
+        uploadList.addAll(uploads);
+        dialog.show(manager, "Admin Applications");
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.Theme_BursaryAdmin);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+            dialog.getWindow().setWindowAnimations(R.style.Theme_BursaryAdmin_Slide);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.admin_view, container, false);
+        recyclerView = view.findViewById(R.id.recycler);
+        toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        adapter = new AdminAdapter(getActivity(), uploadList, new ClickListener() {
+            @Override
+            public void onclick(Upload upload) {
+                new MaterialAlertDialogBuilder(getActivity())
+                        .setTitle("Pick an action")
+                        .setItems(new String[]{"Accept", "Reject"}, (dialog, which) -> {
+                            if (which == 0) {
+                                accept(upload);
+                                dialog.dismiss();
+                            } else {
+                                reject(upload);
+                                dialog.dismiss();
+                            }
+                        }).create().show();
+            }
+        });
+
+        return view;
+    }
+
+    private void accept(Upload upload) {
+        Dialog progressDialog = new Dialog(getActivity());
+        progressDialog.setContentView(R.layout.progressdialog);
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        progressDialog.show();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("requests");
+        acceptListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if (dataSnapshot.child("uploadId").getValue().toString().equals(upload.getUploadId())) {
+                        DatabaseReference updateRef = reference.child(dataSnapshot.getKey()).child("status");
+                        updateRef.setValue("Accepted")
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        progressDialog.dismiss();
+                                        Toast.makeText(getActivity(), "Accepted", Toast.LENGTH_LONG).show();
+                                        reference.removeEventListener(acceptListener);
+                                        getActivity().recreate();
+                                    }
+                                });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(), "An Error Occured", Toast.LENGTH_LONG).show();
+
+            }
+        };
+        reference.addListenerForSingleValueEvent(acceptListener);
+    }
+
+    private void reject(Upload upload) {
+        Dialog progressDialog = new Dialog(getActivity());
+        progressDialog.setContentView(R.layout.progressdialog);
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        progressDialog.show();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("requests");
+        rejectListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if (dataSnapshot.child("uploadId").getValue().toString().equals(upload.getUploadId())) {
+                        DatabaseReference updateRef = reference.child(dataSnapshot.getKey()).child("status");
+                        updateRef.setValue("Rejected")
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        progressDialog.dismiss();
+                                        Toast.makeText(getActivity(), "Rejected", Toast.LENGTH_LONG).show();
+                                        reference.removeEventListener(rejectListener);
+                                        getActivity().recreate();
+                                    }
+                                });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(), "An Error Occured", Toast.LENGTH_LONG).show();
+
+            }
+        };
+        reference.addListenerForSingleValueEvent(rejectListener);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (rejectListener != null) {
+            FirebaseDatabase.getInstance().getReference("requests").removeEventListener(rejectListener);
+        }
+        if (acceptListener != null) {
+            FirebaseDatabase.getInstance().getReference("requests").removeEventListener(acceptListener);
+        }
+    }
 }
--- app/src/main/java/com/jkimtech/bursaryadmin/AdminLogin.java
+++ app/src/main/java/com/jkimtech/bursaryadmin/AdminLogin.java
@@ -2,13 +2,109 @@ package com.jkimtech.bursaryadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class AdminLogin extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AdminLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
+    private EditText editTextEmail, editTextPassword;
+
+    private Button login;
+
+    private FirebaseAuth mAuth;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        login = (Button) findViewById(R.id.buttonLogin) ;
+        login.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        editTextPassword = (EditText) findViewById(R.id.editTextNumberPassword);
+
+        register = findViewById(R.id.adminSignup);
+        register.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonLogin:
+                adminLogin();
+                break;
+
+            case R.id.adminSignup:
+                startActivity(new Intent(AdminLogin.this, AdminSignUp.class));
+                break;
+        }
+    }
+
+    private void adminLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 8) {
+            editTextPassword.setError("Min password length should be 8 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                progressDialog.dismiss();
+                //redirect to user profile
+
+                FirebaseUser admin = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(AdminLogin.this, MainActivity.class));
+
+            } else {
+                progressDialog.dismiss();
+                editTextEmail.setError("Failed to login! Please check your credentials");
+                editTextEmail.requestFocus();
+            }
+        }).addOnFailureListener(e -> {
+            progressDialog.dismiss();
+            Toast.makeText(AdminLogin.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
     }
 }
