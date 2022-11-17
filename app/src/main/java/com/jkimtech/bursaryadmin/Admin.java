8994c9b2c85040a6633221d7097236ebaaefbec1
johnwairia <wairia21@gmail.com>
2022-11-17 12:47:21 +0300

    Initial commit

---
+++ app/src/main/java/com/jkimtech/bursaryadmin/Admin.java
@@ -0,0 +1,4 @@
+package com.jkimtech.bursaryadmin;
+
+public class Admin {
+}
---
+++ app/src/main/java/com/jkimtech/bursaryadmin/AdminSignUp.java
@@ -0,0 +1,14 @@
+package com.jkimtech.bursaryadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_sign_up);
+    }
+}
