d0c0b50fae9a0c2b8de3823a6009e4a418ba567c
johnwairia <wairia21@gmail.com>
2022-11-17 12:59:16 +0300

    update

--- app/src/main/java/com/jkimtech/bursaryadmin/Fetcher.java
+++ app/src/main/java/com/jkimtech/bursaryadmin/Fetcher.java
@@ -1,4 +1,43 @@
 package com.jkimtech.bursaryadmin;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fetcher {
+    private ValueEventListener eventListener, genderListener;
+
+    public void fetchApplications(CompleteListener listener) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("requests");
+        eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                List<Upload> uploadList = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Log.e("data", dataSnapshot.toString());
+                    Upload upload = dataSnapshot.getValue(Upload.class);
+                    uploadList.add(upload);
+                }
+                Log.e("uploadlist", uploadList.toString());
+                reference.removeEventListener(eventListener);
+                listener.onUploadFetched(uploadList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        reference.addListenerForSingleValueEvent(eventListener);
+    }
 }
--- app/src/main/java/com/jkimtech/bursaryadmin/UploadUtils.java
+++ app/src/main/java/com/jkimtech/bursaryadmin/UploadUtils.java
@@ -1,4 +1,113 @@
 package com.jkimtech.bursaryadmin;
 
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
 public class UploadUtils {
+    String downloadUrl;
+
+    public void uploadIdPhoto(Uri idUri, CompleteListener completeListener) {
+        //upload the image to firebase storage
+        //get the download url
+        //call the completeListener.onComplete(downloadUrl)
+
+        StorageReference storage= FirebaseStorage.getInstance().getReference("userids");
+        StorageReference childref=storage.child(new File(idUri.getPath()).getName());
+        UploadTask uploadTask=childref.putFile(idUri);
+
+        Task<Uri> uriTask=uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return childref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                downloadUrl=task.getResult().toString();
+                completeListener.onComplete(downloadUrl);
+            }
+        });
+
+
+    }
+
+    public void uploadfee(Uri fee, CompleteListener completeListener) {
+        //upload the image to firebase storage
+        //get the download url
+        //call the completeListener.onComplete(downloadUrl)
+
+        StorageReference storage = FirebaseStorage.getInstance().getReference("userfees");
+        StorageReference childref = storage.child(new File(fee.getPath()).getName());
+        UploadTask uploadTask = childref.putFile(fee);
+
+        Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return childref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                downloadUrl = task.getResult().toString();
+                completeListener.onComplete(downloadUrl);
+            }
+        });
+    }
+
+    public void uploadCert(Uri fee, CompleteListener completeListener) {
+        //upload the image to firebase storage
+        //get the download url
+        //call the completeListener.onComplete(downloadUrl)
+
+        StorageReference storage = FirebaseStorage.getInstance().getReference("usercerts");
+        StorageReference childref = storage.child(new File(fee.getPath()).getName());
+        UploadTask uploadTask = childref.putFile(fee);
+
+        Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return childref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                downloadUrl = task.getResult().toString();
+                completeListener.onComplete(downloadUrl);
+            }
+        });
+    }
+
+    public void uploadReport(Uri fee, CompleteListener completeListener) {
+        //upload the image to firebase storage
+        //get the download url
+        //call the completeListener.onComplete(downloadUrl)
+
+        StorageReference storage = FirebaseStorage.getInstance().getReference("userreports");
+        StorageReference childref = storage.child(new File(fee.getPath()).getName());
+        UploadTask uploadTask = childref.putFile(fee);
+
+        Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return childref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                downloadUrl = task.getResult().toString();
+                completeListener.onComplete(downloadUrl);
+            }
+        });
+    }
+
 }
